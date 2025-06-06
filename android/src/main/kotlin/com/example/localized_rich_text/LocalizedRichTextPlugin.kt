35972abea17d0dbfd4a951683351536428ffa2a7
Sarthak Parajuli <sarthak.p@acidintegrations.com>
2025-06-06 23:32:03 +0545

    init

--- android/src/main/kotlin/com/example/localized_rich_text/LocalizedRichTextPlugin.kt
+++ android/src/main/kotlin/com/example/localized_rich_text/LocalizedRichTextPlugin.kt
@@ -1,7 +1,5 @@
 package com.example.localized_rich_text
 
-import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -16,12 +14,12 @@ class LocalizedRichTextPlugin: FlutterPlugin, MethodCallHandler {
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "localized_rich_text")
     channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
@@ -29,7 +27,7 @@ class LocalizedRichTextPlugin: FlutterPlugin, MethodCallHandler {
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
 }
--- example/android/app/src/main/kotlin/com/example/localized_rich_text_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/localized_rich_text_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.localized_rich_text_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
